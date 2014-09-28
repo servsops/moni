0f9bb92568f7c4d73ab640fd67fd86bfae7d7f32
gankkank <gankkank@gmail.com>
2014-09-28 22:30:50 +0800

    change to use influxdb 0.8.1

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,8 @@
 #default[:moni][:discovery] = "influxdb"
+
+default[:moni][:influxdb] = {
+  :version => "0.8.1"
+}
 default[:moni][:graphite] = {
   :path => "/opt/graphite",
   :influxdb_path => "127.0.0.1"
--- recipes/_install_influxdb.rb
+++ recipes/_install_influxdb.rb
@@ -2,11 +2,15 @@
 
 path = "http://s3.amazonaws.com/influxdb/influxdb_latest_amd64.deb"
 
+version = node[:moni][:influxdb][:version]
+
+path = "http://s3.amazonaws.com/influxdb/influxdb_#{version}_amd64.deb"
+
 bash "install influxdb package" do
   cwd "/tmp"
   code <<-EOH
     wget #{path}
-    dpkg -i influxdb_latest_amd64.deb
+    dpkg -i influxdb_#{version}_amd64.deb
     EOH
   not_if "which influxdb"
 end
